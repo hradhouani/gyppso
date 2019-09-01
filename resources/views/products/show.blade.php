709ed410a3bd7bd8c65e53d356d4488067f8958d
haythem radhoini <haythamov1993@gmail.com>
2019-09-01 09:52:43 +0100

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,10 +8,7 @@
                     <div class="row">
                         <div class="col-md-12 text-center">
                             <div class="slides-text probootstrap-animate" data-animate-effect="fadeIn">
-                                <h2>About Us</h2>
-                                <p>Free HTML5 Template by <a href="https://uicookies.com/"
-                                                             target="_blank">uicookies.com</a>. Far far away, behind the
-                                    word mountains, far from the countries.</p>
+                                
                             </div>
                         </div>
                     </div>
@@ -55,7 +52,7 @@
 
                 </div>
                 <div class="col-md-4">
-                <h1>{{$product->title}}</h1>
+                <h1 class="font-weight-bold">{{$product->title}}</h1>
                 <p>{!! $product->body !!}</p>
                 </div>
             </div>
